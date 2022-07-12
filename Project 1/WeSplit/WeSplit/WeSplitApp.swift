20383709dae9999567eed72343a1e554aac75d10
Andrei Rybak <Andrei_Rybak@epam.com>
2022-07-12 13:04:17 +0200

    Finished Project 1- WeSplit. Day 16, 17, 18.

---
+++ Project 1/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Andrei Rybak on 12.07.22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
