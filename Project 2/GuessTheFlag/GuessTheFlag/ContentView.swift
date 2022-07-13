f0662931327874625805b6f082c4ead689f922c5
Andrei Rybak <Andrei_Rybak@epam.com>
2022-07-13 12:45:44 +0200

    Challenge 2 day 24. Using a custom view.

--- Project 2/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ Project 2/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -7,6 +7,17 @@
 
 import SwiftUI
 
+struct FlagImage: View {
+    let imageName: String
+
+    var body: some View {
+        Image(imageName)
+            .renderingMode(.original)
+            .clipShape(Capsule())
+            .shadow(radius: 5)
+    }
+}
+
 struct ContentView: View {
     @State private var showingScore = false
     @State private var restarTheGame = false
@@ -46,10 +57,7 @@ struct ContentView: View {
                         Button {
                             flagTapped(number)
                         } label: {
-                            Image(countries[number])
-                                .renderingMode(.original)
-                                .clipShape(Capsule())
-                                .shadow(radius: 5)
+                            FlagImage(imageName: countries[number])
                         }
                     }
                 }
