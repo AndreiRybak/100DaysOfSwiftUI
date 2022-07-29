78cc01842266ce8061496109be616be9b784f078
Andrei Rybak <andreyrybak0895@gmail.com>
2022-07-29 13:48:15 +0200

    Project 15 Accessibility done. Day 74,75,76.

---
+++ Project 15/AccessibilitySandbox/AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by Andrei Rybak on 29.07.22.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
