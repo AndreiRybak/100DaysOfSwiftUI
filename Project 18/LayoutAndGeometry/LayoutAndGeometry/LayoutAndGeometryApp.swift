0276341abc71607d0424645d7c4f608d646ff87f
Andrei Rybak <andreyrybak0895@gmail.com>
2022-08-03 11:05:42 +0200

    Project 18 Layout and Geometry completed.

---
+++ Project 18/LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Andrei Rybak on 3.08.22.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
